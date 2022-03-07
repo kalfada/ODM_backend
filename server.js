f200a1000ae13e2a2c9f8aad74b8e031b4ea60f1
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2022-03-07 02:26:52 +0200

    update

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('./db')
 
 const
     express = require('express')
-    app = express()
-    PORT = 3000;
-    router = require('./router')
+app = express()
+PORT = 3000;
+router = require('./router')
 
 app.use(express.json())
 app.use(require('cors')())
 
 router(app)
 
-app.listen(PORT, ()=> console.log(`server is running => ${PORT}`))
+app.listen(process.env.PORT || PORT, () => console.log(`server is running => ${PORT}`))
