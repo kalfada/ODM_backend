a2cef885c7b7fadbca67b13200f726a4e70b134b
Daniel Kalfa <49788440+kalfada@users.noreply.github.com>
2022-06-16 13:32:46 +0300

    update

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-const connectionString = `mongodb+srv://kalfada:Mottiemmanuelle123@cluster0.d48l6.mongodb.net/odmDB?retryWrites=true&w=majority`
+const connectionString = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.d48l6.mongodb.net/odmDB?retryWrites=true&w=majority`
     
 mongoose.connect(connectionString)
     .then(res => {
